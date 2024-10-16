45d9be99000bc4a43e26c53be0bf28ad661896dd
harshalDev <harshalwarade344@gmail.com>
2024-10-16 17:47:50 +0530

    add code

--- server/controllers/application.controlelrs.js
+++ server/controllers/application.controlelrs.js
@@ -47,3 +47,94 @@ export const applyJob = async (req, res) => {
     }
 }
 
+export const getAppliedJobs = async(req, res) => {
+    try {
+        const userId = req.id
+        const application = await Application.find({applicant: userId}).sort({createdAt: -1}).populate({
+            path: 'job',
+            options: {sort: {createdAt: -1}},
+            populate: {
+                path: 'company',
+                options: {sort: {createdAt: -1}},
+            }
+        })
+        if(!application) {
+            return res.status(404).json({
+                message: "No application!",
+                success: false
+            })
+        }
+
+        res.status(200).json({
+            application,
+            success: true
+        })
+
+    } catch (err) {
+        console.log(`error occured at getAppliedJobs controller backend: ${err}`)
+    }
+}
+
+
+export const getApplicants = async (req, res) => {
+    try {
+        const jobId = req.params.id
+        const job = await Job.findById(jobId).populate({
+            path: 'applications',
+            options: {sort: {createdAt: -1}},
+            populate: {
+                path: 'applicant'
+            }
+        })
+
+        if(!job) {
+            return res.status(404).json({
+                message: "Job not found!",
+                success: false
+            })
+        }
+
+        return res.status(200).json({
+            job,
+            success: true
+        })
+
+    } catch (err) {
+        console.log(`error occured at getApplicants controller backend: ${err}`)
+    }
+}
+
+
+
+export const updateStatus = async (req, res) => {
+    try {
+        const {status} = req.body
+        const applicationId = req.params.id
+        if(!status) {
+            return res.status(404).json({
+                message: "Status is required!",
+                success: false
+            })
+        }
+
+        const application = await Application.findOne({_id: applicationId})
+        if(!application) {
+            return res.status(404).json({
+                message: "Application not found!",
+                success: false
+            })
+        }
+
+        application.status = status.toLowerCase()
+
+        await application.save()
+
+        return res.status(200).json({
+            message: "Status updated successfully!",
+            success: true
+        })
+
+    } catch (err) {
+        console.log(`error occured at updateStatus controller backend: ${err}`)
+    }
+}
