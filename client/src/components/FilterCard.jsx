d0b3ad54e302285215a011c34dcf3afd296b921a
harshalDev <harshalwarade344@gmail.com>
2024-12-27 15:24:57 +0530

    make major changes

--- client/src/components/FilterCard.jsx
+++ client/src/components/FilterCard.jsx
@@ -1,9 +1,46 @@
 import React from 'react'
+import { RadioGroup, RadioGroupItem } from './ui/radio-group'
+import { Label } from './ui/label'
+
+const FilterData = [
+  {
+    filterType: "Location",
+    array: ["Delhi", "Bangalore", "Hydrabad", "Pune", "Chennai", "Mumbai"]
+  },
+  {
+    filterType: "Industry",
+    array: ["Frontend Developer", "Backend Developer", "Fullstack Developer"]
+  },
+  {
+    filterType: "Salary",
+    array: ["0-40k", "40k-80k", "80k-1.2L", "1.2L-1.6L", "1.6L and above"]
+  },
+]
 
 const FilterCard = () => {
   return (
-    <div>
-      Filter Card
+    <div className='w-full bg-white p-3 rounded-md'>
+      <h1 className='font-bold text-lg'>Filter Jobs</h1>
+      <hr className='mt-3' />
+      <RadioGroup>
+        {
+          FilterData.map((data, index) => (
+            <div>
+              <h1 className='font-bold text-lg'>{data.filterType}</h1>
+              {
+                data.array.map((item, index) => {
+                  return (
+                    <div className='flex items-center space-x-2 m-2'>
+                      <RadioGroupItem value={item} />
+                      <Label>{item}</Label>
+                    </div>
+                  )
+                })
+              }
+            </div>
+          ))
+        }
+      </RadioGroup>  
     </div>
   )
 }
