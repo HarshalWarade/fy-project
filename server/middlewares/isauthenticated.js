8753b7cacd9a4f20b1f560ae41772536e0965804
harshalDev <harshalwarade344@gmail.com>
2025-02-27 15:34:05 +0530

    comment consoles

--- server/middlewares/isauthenticated.js
+++ server/middlewares/isauthenticated.js
@@ -5,30 +5,30 @@ const isAuthenticated = async(req, res, next) => {
     try {
         
         const token = req.cookies.token
-        console.log("Check 1")
+        // console.log("Check 1")
         if(!token) {
             return res.status(401).json({
                 message: "Unauthorised access is not allowed!",
                 success: false
             })
         }
-        console.log("Check 2")
+        // console.log("Check 2")
         
         const decode = await jwt.verify(token, process.env.SECRETKEY)
-        console.log("Check 3")
+        // console.log("Check 3")
         if(!decode) {
             return res.status(401).json({
                 message: "Invalid Token!",
                 success: false
             })
         }
-        console.log("Check 4")
+        // console.log("Check 4")
         
         req.id = decode.userId
-        console.log("Check 5")
+        // console.log("Check 5")
         
         next()
-        console.log("Check 6")
+        // console.log("Check 6")
 
     } catch (err) {
         console.log(`Error at isAuthenticated file: ${err}`)
